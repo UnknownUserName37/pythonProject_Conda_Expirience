385493f98f063a9c3275fa38bfccbcc7ee21f32e
Nappa <kikbox3112@gmail.com>
2022-10-21 21:23:41 +0300

    My_Begining

--- vklad_Task.py
+++ vklad_Task.py
@@ -1,6 +1,6 @@
 #можно юзать как схему для рассчёта крипты. sum_profit - сколько хочешь, sum_dup - скок имеешь. count - времени потребуется.
-p = 1.1
-count = 0
+p = 0.1
+count = 1
 tupo = 0
 while True:
 
@@ -8,14 +8,13 @@ while True:
     sum_profit = float(input("Скок хочешь? -> "))
     while True:
         tupo += sum_dep * p
-        och_tupo = sum_dep * p
 
-        print(sum_dep, "я деп")
+        print(tupo, "я деп")
         print(sum_profit, 'я профит')
-        if (sum_profit // tupo) == 0:
-            years = sum_profit / och_tupo
-            print("потерпи стока лет -> ", count, "у тя будет стока бабла -> ", tupo)
+        count += 1
 
+        if (sum_profit // tupo) == 0:
+            years = sum_profit / tupo
+            print("потерпи стока лет -> ", years, "у тя будет стока бабла -> ", tupo)
             break
 
-        count += 1
