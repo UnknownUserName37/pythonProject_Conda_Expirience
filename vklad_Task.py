706f5bd9964778add97b6f8801c688f9efae0220
Nappa <kikbox3112@gmail.com>
2022-10-21 21:01:17 +0300

    My_Begining

--- vklad_Task.py
+++ vklad_Task.py
@@ -6,15 +6,16 @@ while True:
 
     sum_dep = float(input("Твой стартовый деп? -> "))
     sum_profit = float(input("Скок хочешь? -> "))
-    while sum_dep <= sum_profit:
+    while True:
         tupo += sum_dep * p
         och_tupo = sum_dep * p
 
         print(sum_dep, "я деп")
         print(sum_profit, 'я профит')
-        count += 1
         if (sum_profit // tupo) == 0:
             years = sum_profit / och_tupo
-            print("потерпи стока лет -> ", years, "у тя будет стока бабла -> ", tupo)
+            print("потерпи стока лет -> ", count, "у тя будет стока бабла -> ", tupo)
 
             break
+
+        count += 1
