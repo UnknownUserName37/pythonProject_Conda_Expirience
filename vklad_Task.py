b2b6219c4992b6a6f0043d99e3e9d079e5a03944
Nappa <kikbox3112@gmail.com>
2022-10-22 12:44:20 +0300

    My_Begining

--- vklad_Task.py
+++ vklad_Task.py
@@ -6,8 +6,8 @@ och_tupo = 0
 
 while True:
 
-    sum_dep = int(input("Твой стартовый деп? -> "))
-    sum_profit = int(input("Скок хочешь? -> "))
+    sum_dep = float(input("Твой стартовый деп? -> "))
+    sum_profit = float(input("Скок хочешь? -> "))
 
     while True:
         och_tupo = sum_dep * p #сколько от депозита в год
@@ -18,7 +18,7 @@ while True:
 
         if (sum_profit // tupo) == 0:
             print(tupo, "я новый деп")
-            years = ((sum_profit - (och_tupo*10)) / och_tupo) #рассчёт бабок без учёта начальной суммы депозита
-            print("потерпи стока лет -> ", float(years), "у тя будет стока бабла -> ", int(tupo))
+            years = ((sum_profit - (och_tupo*10)) / och_tupo) #рассчёт бабок с учётом суммы начального депозита
+            print("потерпи стока лет -> ", float(years), "у тя будет стока бабла -> ", float(tupo))
             break
 
