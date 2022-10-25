684c9cd92eca15b85fd4daf013b2845ecd638a32
Nappa <kikbox3112@gmail.com>
2022-10-25 13:42:19 +0300

    My_Begining

--- script.py
+++ script.py
@@ -5,8 +5,8 @@ from selenium.webdriver.chrome.options import Options as ChromOptions
 import chromedriver_binary
 
 def run_script():
-
     options = ChromOptions()
+    # Не открывает браузер, а делает всё в фоне. options.headless = True
     driver = Chrome(options=options)
     driver.get("https://skillbox.ru")
     driver.quit()
---
+++ test_test.py
@@ -0,0 +1,15 @@
+#Remoute Auto_Test #2
+
+from selenium.webdriver import Remote
+
+class TestExample: #TestSuit
+
+    def test_test(self): #TestCase
+
+        driver = Remote(desired_capabilities={
+            "browserName": "chrome",
+            "browserVersion": "latest"
+        }, command_executor="http://127.0.0.1:4444/wd/hub")
+        driver.get("https://skillbox.ru")
+
+        driver.quit()
