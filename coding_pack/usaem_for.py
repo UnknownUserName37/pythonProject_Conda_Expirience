cf98697f901201bc3045f4a90672bef9da02b263
Nappa <kikbox3112@gmail.com>
2022-10-27 18:09:35 +0300

    My_Begining

--- coding_pack/usaem_for.py
+++ coding_pack/usaem_for.py
@@ -5,6 +5,7 @@ for i in 0, 1, 2, 3, 4, 5:
 
 #Lol_task#2
 count = 0
+
 for joint in 33, 11, 49, 92, 534, 840, 1260:
    if (joint % 420) == 0:
     print('lucky 420 x5 free BUDS, his # ->', joint)
@@ -19,14 +20,25 @@ for num in 3, 4, 5, 6, 7:
 #Real_Hardly_Task #4
 tickets_luck_hund = 0
 count_1 = 1
+
 for tickets_luck in 33, 442, 777, 666, 420:
     if tickets_luck >= 100 and tickets_luck <= 999 and tickets_luck % 5 == 0:
         print("You are lucky", tickets_luck)
 
 #Easy_Task_again #5
 text = "Python!"
+
 for fifth in range (5):
     print("Hi, dudes")
 
 #Do_Tasks
-month = int(input("Сколько будем по времени ужиматься в деньгах? "))
+
+time = int(input("Сколько будем по времени ужиматься в деньгах? -> ☼ -> "))
+money = int(input("Скок откидываешь?? ->> "))
+summ = 0
+
+for month in range(time):
+    print("Это месяц", month+1)
+    summ = money * time
+    print("ПОздравляю, у тебя", summ, "бабок ♀ и ты потратил на это всего", month + 1, "месяца :-D")
+
