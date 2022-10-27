3ce6cea27fa635f028327e63b89274a6a192bbce
Nappa <kikbox3112@gmail.com>
2022-10-27 17:43:58 +0300

    My_Begining

--- coding_pack/usaem_for.py
+++ coding_pack/usaem_for.py
@@ -15,3 +15,18 @@ for joint in 33, 11, 49, 92, 534, 840, 1260:
 #P.S. Easy_Task
 for num in 3, 4, 5, 6, 7:
     print(num **3, num ** 4, num ** 5)
+
+#Real_Hardly_Task #4
+tickets_luck_hund = 0
+count_1 = 1
+for tickets_luck in 33, 442, 777, 666, 420:
+    if tickets_luck >= 100 and tickets_luck <= 999 and tickets_luck % 5 == 0:
+        print("You are lucky", tickets_luck)
+
+#Easy_Task_again #5
+text = "Python!"
+for fifth in range (5):
+    print("Hi, dudes")
+
+#Do_Tasks
+month = int(input("Сколько будем по времени ужиматься в деньгах? "))
