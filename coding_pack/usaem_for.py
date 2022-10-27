754de263d93241b668caef80196cf0d99c06990d
Nappa <kikbox3112@gmail.com>
2022-10-27 16:42:08 +0300

    My_Begining

---
+++ coding_pack/usaem_for.py
@@ -0,0 +1,17 @@
+#lOL_task
+
+for i in 0, 1, 2, 3, 4, 5:
+    print(i / 2)
+
+#Lol_task#2
+count = 0
+for joint in 33, 11, 49, 92, 534, 840, 1260:
+   if (joint % 420) == 0:
+    print('lucky 420 x5 free BUDS, his # ->', joint)
+    count += 1
+    print("our luckers", count)
+
+# Hardly_Task #3
+#P.S. Easy_Task
+for num in 3, 4, 5, 6, 7:
+    print(num **3, num ** 4, num ** 5)
