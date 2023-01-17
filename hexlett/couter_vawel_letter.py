a8ab66b3a79bf342fd3cd569404630622355f06f
Nappa <kikbox3112@gmail.com>
2023-01-18 00:05:59 +0300

    My_Begining
    new_hexlett_tasks

--- hexlett/couter_vawel_letter.py
+++ hexlett/couter_vawel_letter.py
@@ -1,11 +1,11 @@
-from symbols import is_vowel #  Из курса Hexlett. Там (в их интерпритаторе) она работала.
+# from symbols import is_vowel #  Из курса Hexlett. Там (в их интерпритаторе) она работала.
 
 
-def count_vowels(text):
-    vowel_letter_counter_in_text = is_vowel(text)
-    counter_vowel_lette_in_text = 1
-
-    for vowel_letter_counter_in_text in text:
-        counter_vowel_lette_in_text += 1
-
-        return counter_vowel_lette_in_text
+# def count_vowels(text):
+#     vowel_letter_counter_in_text = is_vowel(text)
+#     counter_vowel_lette_in_text = 1
+#
+#     for vowel_letter_counter_in_text in text:
+#         counter_vowel_lette_in_text += 1
+#
+#         return counter_vowel_lette_in_text
